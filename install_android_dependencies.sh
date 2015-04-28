fbd1b3f6a11499c3fadd51e2386b96b49ede6de0
Falko Richter <falko@sensorberg.com>
2015-04-28 13:49:58 +0200

    added android support

--- install_android_dependencies.sh
+++ install_android_dependencies.sh
@@ -4,5 +4,6 @@ set +e
 
 echo y | android update sdk --no-ui --filter extra-android-m2repository > /dev/null
 echo y | android update sdk --no-ui --filter extra-google-m2repository > /dev/null
+echo y | android update sdk --no-ui --filter extra-android-support > /dev/null
 echo y | android update sdk --no-ui --filter android-18 > /dev/null
 echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-18 --all > /dev/null
