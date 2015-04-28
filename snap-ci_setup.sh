c02e597bd6721e97afefe4e205b9e9a995c5c536
Falko Richter <falko@sensorberg.com>
2015-04-28 13:28:39 +0200

    more android dependencies

--- snap-ci_setup.sh
+++ snap-ci_setup.sh
@@ -3,6 +3,8 @@
 set +e
 
 download-android
+echo y | android update sdk --no-ui --filter extra-android-support > /dev/null
+echo y | android update sdk --no-ui --filter extra-google-m2repository > /dev/null
 echo y | android update sdk --no-ui --filter android-18 > /dev/null
 echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-18 --all > /dev/null
 
